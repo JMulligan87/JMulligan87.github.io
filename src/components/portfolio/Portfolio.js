f2a290c25e8125fc69fa8b95a95a8925430425f2
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2020-02-06 21:02:10 -0500

    mapped cards to the portfolio section from a json file

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  Card,
+  Container,
+  Col,
+  Row,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardImg
+} from 'reactstrap';
+import projects from '../../projects.json';
 
-const Portfolio = () => {
-  return <section id='portfolio'>hi</section>;
-};
+class Portfolio extends Component {
+  state = {
+    projects: [...projects]
+  };
+  render() {
+    return (
+      <section id='portfolio'>
+        <Container>
+          <Row>
+            {this.state.projects.map(project => (
+              <Col lg='4' sm='6'>
+                <div>
+                  <Card className='text-center'>
+                    <CardImg
+                      top
+                      height='100%'
+                      width='100%'
+                      src={project.image}
+                      alt={project.title}
+                    />
+                    <CardBody>
+                      <CardTitle>{project.title}</CardTitle>
+                      <CardText>{project.description}</CardText>
+                    </CardBody>
+                  </Card>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+    );
+  }
+}
 
 export default Portfolio;
