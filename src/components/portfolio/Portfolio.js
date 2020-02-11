0a2154bcc4f31a50e6cba8f51e9b7425f989cda7
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2020-02-11 00:46:01 -0500

    got modals working for the portfolio cards

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Card,
   Container,
@@ -8,11 +8,17 @@ import {
   CardTitle,
   CardText,
   CardImg,
-  Button
+  Button,
+  Modal,
+  ModalBody,
+  ModalFooter
 } from 'reactstrap';
 import projects from '../../projects.json';
 
 const Portfolio = () => {
+  const [modal, setModal] = useState(false);
+  const toggle = project => setModal(project.id);
+
   return (
     <section id='portfolio'>
       <Container>
@@ -30,25 +36,60 @@ const Portfolio = () => {
                   />
                   <CardBody>
                     <CardTitle>{project.title}</CardTitle>
-                    <CardText>{project.description}</CardText>
+                    <CardText className='text-muted'>
+                      {project.description}
+                    </CardText>
                     <Row className='justify-content-center'>
-                      <Button
-                        color='info'
-                        className='mr-2 d-flex align-items-center'
-                        href={project.href}
-                        target='_blank'
-                        rel='noopener noreferrer'
-                      >
-                        Check it out
+                      <Button color='info' onClick={() => toggle(project)}>
+                        More Info
                       </Button>
-                      <a
-                        className='d-block'
-                        href={project.github}
-                        target='_blank'
-                        rel='noopener noreferrer'
+                      <Modal
+                        isOpen={modal === project.id}
+                        toggle={toggle}
+                        size='xl'
                       >
-                        <i className='fab fa-github fa-3x text-muted'></i>
-                      </a>
+                        <ModalBody size='lg'>
+                          <CardImg
+                            className='img-fluid'
+                            size='xl'
+                            style={{
+                              width: '100%',
+                              height: '100%'
+                            }}
+                            title='resume'
+                            alt={project.title}
+                            src={require('../../img' + project.image)}
+                          />
+                          <p className='mt-4'>{project.description}</p>
+                        </ModalBody>
+                        <ModalFooter>
+                          <Button
+                            color='info'
+                            className='mr-2 d-flex '
+                            href={project.href}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                          >
+                            Check it out
+                          </Button>
+                          <a
+                            className='d-block mr-2'
+                            href={project.github}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                          >
+                            <i className='fab fa-github fa-3x text-muted'></i>
+                          </a>
+                          <Button
+                            color='danger'
+                            className='btn btn-primary btn-xl'
+                            size='md'
+                            onClick={toggle}
+                          >
+                            Close
+                          </Button>
+                        </ModalFooter>
+                      </Modal>
                     </Row>
                   </CardBody>
                 </Card>
@@ -60,5 +101,60 @@ const Portfolio = () => {
     </section>
   );
 };
+
+// const ProjectModal = () => {
+//   const [modal, setModal] = useState(project.id);
+//   const toggle = () => setModal(!modal);
+
+//   return (
+//     <div>
+//       <Button color='info' onClick={toggle}>
+//         My Resume
+//       </Button>
+//       <Modal isOpen={modal} toggle={toggle} size='lg'>
+//         <ModalBody size='lg'>
+//           <img
+//             className='img-fluid'
+//             style={{
+//               width: '100%',
+//               height: '100%'
+//             }}
+//             title='resume'
+//             alt={project.title}
+//             src={require('../../img' + project.image)}
+//           />
+//           <p>{project.description}</p>
+//         </ModalBody>
+//         <ModalFooter>
+//           <Button
+//             color='info'
+//             className='mr-2 d-flex align-items-center'
+//             href={project.href}
+//             target='_blank'
+//             rel='noopener noreferrer'
+//           >
+//             Check it out
+//           </Button>
+//           <a
+//             className='d-block'
+//             href={project.github}
+//             target='_blank'
+//             rel='noopener noreferrer'
+//           >
+//             <i className='fab fa-github fa-3x text-muted'></i>
+//           </a>
+//           <Button
+//             color='secondary'
+//             className='btn btn-primary btn-xl bg-info'
+//             size='sm'
+//             onClick={toggle}
+//           >
+//             Close
+//           </Button>
+//         </ModalFooter>
+//       </Modal>
+//     </div>
+//   );
+// };
 
 export default Portfolio;
